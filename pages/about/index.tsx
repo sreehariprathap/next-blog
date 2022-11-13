b8fb653b2d72c2f1181e138f215d4ffd2fe1abfd
Sreehari Prathap <sreehariprathap1996@gmail.com>
2022-11-13 22:46:50 +0530

    font changes

---
+++ pages/about/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+        <div>
+            <h2>About Blog</h2>
+        </div>
+    </div>
+  )
+}
+
+export default index
